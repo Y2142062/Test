5f2e8b2a78335960ceadfde03502de787f5b094d
takasaki <hidelego17@gmail.com>
2023-06-13 14:21:14 +0900

    new file

---
+++ in2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>商品一覧</title>
+	</head>
+	<body>
+	    <form method="post" action="rec2.php">
+        <input type="password" name="passdayo">
+        <input type="submit" value="送信">
+        </form>
+	</body>
+</html>
